a91a0ff0d341256f7710c6bc0dd79d67381e92f4
Mutaz Lafi <mutazhp@hotmail.com>
2023-06-03 20:28:20 +0300

    Write Some Code

--- Clock_Project.ino
+++ Clock_Project.ino
@@ -1,9 +1,25 @@
+// Libraries
+#include <Wire.h>
+#include "RTClib.h"
+#include "dht.h"
+#include <Liquid_Crystal_I2C.h>
+// Defines And Constant Variables
+#define DHTPIN 9
+#define DHTTYPE DHT11
+
+//Variables
+
+// Objects
+RTC_DS3231 RTC;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+Liquid_Crystal_I2C lcd(16, 2, 0x24);   // LCD Width , LCD height, LCD I2C Address 
+
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
